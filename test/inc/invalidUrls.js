5fd15a8d4fb328462e7361268c6a355385faa82a
Graham F. Scott <gfscott@gmail.com>
2022-11-10 20:18:41 -0500

    Rome formatting fixes

--- test/inc/invalidUrls.js
+++ test/inc/invalidUrls.js
@@ -1,17 +1,17 @@
 /**
  * INVALID URLS
- * 
+ *
  * Starting with the list of valid URLs, return a list of
  * invalid strings with text prepended or appended.
  */
 
-const validUrls = require('./validUrls.js');
+const validUrls = require("./validUrls.js");
 
-const prependedText = validUrls.map(url => {
-  return `Foo ${url}`;
+const prependedText = validUrls.map((url) => {
+	return `Foo ${url}`;
 });
-const appendedText = validUrls.map(url => {
-  return `${url} Foo`;
+const appendedText = validUrls.map((url) => {
+	return `${url} Foo`;
 });
 
 const invalidUrls = prependedText.concat(appendedText);
--- test/inc/validUrls.js
+++ test/inc/validUrls.js
@@ -5,24 +5,33 @@
  * all possible valid URL permutations.
  */
 
-const permute = require('./permuteArrays.js');
+const permute = require("./permuteArrays.js");
 
 /**
  * Core URL structures accepted by the plugin
  * Domain and path only
  */
 const validUrls = [
-  'soundcloud.com/earlxsweatshirtmusic',
-  'soundcloud.com/earlxsweatshirtmusic/tisktisk-cookies',
-  'soundcloud.com/earlxsweatshirtmusic/sets/earl-15'
-]
+	"soundcloud.com/earlxsweatshirtmusic",
+	"soundcloud.com/earlxsweatshirtmusic/tisktisk-cookies",
+	"soundcloud.com/earlxsweatshirtmusic/sets/earl-15",
+];
 
 /**
  * Non-core URL structures accepted by the plugin
  * Various protocol variants and URL parameters
  */
-const validPrefixes = ['', '//', 'http://', 'https://', 'www.', '//www.', 'http://www.', 'https://www.']
-const validSuffixes = ['?', '?foo', '?foo=bar']
+const validPrefixes = [
+	"",
+	"//",
+	"http://",
+	"https://",
+	"www.",
+	"//www.",
+	"http://www.",
+	"https://www.",
+];
+const validSuffixes = ["?", "?foo", "?foo=bar"];
 
 /**
  * Cumulative lists of all URL permutations.
